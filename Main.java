d646cf75119f1b579b32525723ffa8e2a9dc06bc
ddm <ilmif303@gmail.com>
2025-12-15 10:55:53 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package StaicFinal;
+
+public class Main {
+    public static void main(String[] args) {
+        Test test = new Test();
+        System.out.println(test.num1);
+        test.num11();
+        test.sc.nextLine();
+        System.out.println(test.countMoneyD_billion);
+        // test.countMoneyD_billion = 122; // так нельзя
+    }
+}
---
+++ Main2.java
@@ -0,0 +1,11 @@
+package StaicFinal;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println(Test.num2);
+        Test.num22();
+        Test.scanner.nextLine();
+      //  Test.moneyD_billionsTwo = 23;
+        System.out.println(Test.moneyD_billionsTwo);
+    }
+}
